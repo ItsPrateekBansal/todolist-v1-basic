5126ccb81a91a4898d0bec65ccbd7e3591213f5d
Prateek Bansal <prateekbansal64@gmail.com>
2020-07-12 23:23:26 +0530

    added work list section

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var items = [];
+var workItems = [];
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,15 +18,37 @@ app.get("/", function(req, res) {
   }
 
   res.render('list', {
-    kindOfday: today.toLocaleDateString("hi-IN", options),
-    newListItem:items,
+    listItem: today.toLocaleDateString("hi-IN", options),
+    newListItems:items,
+    listType:"home"
   });
 });
 
 app.post("/", function(req, res) {
+  var type = req.body.button;
   var item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  console.log(item)
+  if(type === 'home'){
+    items.push(item);
+    res.redirect("/");
+  }
+  else if(type == 'work'){
+    workItems.push(item);
+    res.redirect("/work");
+  }
+
+})
+
+
+
+app.get("/work",function(req,res){
+  // res.render({listItem: "Work List",newListItems:workItems});
+    var today = new Date();
+  res.render('list',{
+    listItem:"Work List",
+    newListItems:workItems,
+    listType:"work"
+  });
 })
 
 app.listen(3000, (req, res) => {
